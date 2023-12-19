d1fa473830e3077e5d51e4686d601fa292e9ef27
Rodrigo Fernandes <me@rodrigofernand.es>
2023-12-18 23:56:42 -0300

    first storybook file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,13 @@ import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   staticDirs: ['../public'],
-  stories: ['../src/components/**/stories.@(ts|tsx)'],
+  stories: ['../src/components/**/*.stories.(ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-styling-webpack'
   ],
   framework: {
     name: '@storybook/nextjs',
