6705c6751145994516d4a82a856cf31b2672699a
Rodrigo Fernandes <me@rodrigofernand.es>
2023-12-21 15:32:15 -0300

    font

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,6 @@
 export const Main = () => (
   <main className="bg-gray-900 text-white w-full h-full p-12 text-center flex flex-col items-center justify-center">
     <h1>React avançado</h1>
+    <p>teeeeeeest</p>
   </main>
-)
+);
