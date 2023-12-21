3c2b54cf08fb09496511798d40cc341584463a72
Rodrigo Fernandes <me@rodrigofernand.es>
2023-12-21 16:35:05 -0300

    some changes

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,6 @@
 export const Main = () => (
   <main className="bg-gray-900 text-white w-full h-full p-12 text-center flex flex-col items-center justify-center">
-    <h1>React avançado</h1>
-    <p>teeeeeeest</p>
+    <h1 className="text-black">React avançado</h1>
+    <p className="text-black">teeeeeeest</p>
   </main>
-)
+);
