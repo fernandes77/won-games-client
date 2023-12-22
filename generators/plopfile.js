1269c5f947075940606924006467ec565b502dfe
Rodrigo Fernandes <me@rodrigofernand.es>
2023-12-21 21:34:59 -0300

    generators

---
+++ generators/plopfile.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
+module.exports = (plop) => {
+  plop.setGenerator('component', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/Component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.ts',
+        templateFile: 'templates/Component.stories.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+        templateFile: 'templates/Component.test.tsx.hbs'
+      }
+    ]
+  })
+}
