995a0fe31aa19fe3cbd33c8123670948095fb7b6
Rodrigo Fernandes <me@rodrigofernand.es>
2023-12-22 03:31:37 -0300

    rm Main

--- src/components/Main/Main.tsx
+++
@@ -1,6 +0,0 @@
-export const Main = () => (
-  <main className="bg-gray-900 text-white w-full h-full p-12 text-center flex flex-col items-center justify-center">
-    <h1 className="text-black">React avançado</h1>
-    <p className="text-black">teeeeeeest</p>
-  </main>
-);
